a50c1e3a2e0ed8e63e8c40239f2d1596d3b540f1
YongHoonJJo <starctak@gmail.com>
2018-12-09 22:28:11 +0900

    using 'iconv' module for encoding utf-8

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const app = express();
 
 const request = require('request');
 const cheerio = require('cheerio');
+const Iconv = require('iconv').Iconv;
+const iconv = new Iconv('CP949', 'utf-8//translit//ignore');
 
 app.get("/crawlerPrac", (req, res) => {
     const url = "http://movie.naver.com/movie/sdb/rank/rmovie.nhn";
 
-    request(url, (err, response, body) => {
+    request({url, encoding: null}, (err, response, body) => {
+        let htmlDoc = iconv.convert(body).toString();
         let results = [];
 
-        const $ = cheerio.load(body);
+        const $ = cheerio.load(htmlDoc);
         let colArr = $('.tit3');
         for(let i=0; i<colArr.length; i++)
             results.push(colArr[i].children[1].attribs.title);
