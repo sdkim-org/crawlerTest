ebb5ec0f02846db899d2641c23a4d2a79fc971d9
YongHoonJJo <starctak@gmail.com>
2018-12-09 22:13:46 +0900

    using 'cheerio' module

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 
 const request = require('request');
+const cheerio = require('cheerio');
 
-app.get("/crawlerPrac", function (req, res) {
-    const url = 'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html';
+app.get("/crawlerPrac", (req, res) => {
+    const url = "http://movie.naver.com/movie/sdb/rank/rmovie.nhn";
 
-    request(url, function(err, response, body) {
-        console.log(body);
+    request(url, (err, response, body) => {
+        let results = [];
+
+        const $ = cheerio.load(body);
+        let colArr = $('.tit3');
+        for(let i=0; i<colArr.length; i++)
+            results.push(colArr[i].children[1].attribs.title);
+        res.json(results);
     });
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log('listening on port 3000');
 });
